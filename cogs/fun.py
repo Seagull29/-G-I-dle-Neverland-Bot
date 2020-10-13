30bf456656ab402a13284690b2eca3bb787eeb62
Erian <54561501+Seagull29@users.noreply.github.com>
2020-10-13 09:37:41 -0500

    add menu to youtube command

--- cogs/fun.py
+++ cogs/fun.py
@@ -5,6 +5,7 @@ from typing import Optional
 import requests, json, aiohttp
 from discord.ext.menus import ListPageSource, MenuPages
 from datetime import datetime
+import urllib, re
 
 class Fun(Cog):
     def __init__(self, bot):
@@ -36,6 +37,19 @@ class Fun(Cog):
         await session.close()
         await ctx.send(embed = embed)
 
+    @command(name = "yt", aliases = ["youtube"])
+    async def youtube_search(self, ctx, *, video):
+        if len(video) == 0:
+            await ctx.send("Indique que desea buscar")
+        else:
+            search_keyword = video.replace(" ", "+")
+            html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
+            video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+            urls = [[url] for url in video_ids]
+            menu = MenuPages(source = BusquedaYoutube(ctx, urls), delete_message_after = False)
+            await menu.start(ctx)
+            #await ctx.send("https://www.youtube.com/watch?v=" + video_ids[0])
+
 
     async def random_gif(self, ctx, url):
         longitud_codigo = 31 + url[31:].find("/")
@@ -66,7 +80,6 @@ class Fun(Cog):
         await session.close()
     
 
-
 class Paginas(ListPageSource):
     def __init__(self, ctx, data, busqueda):
         self.ctx = ctx
@@ -84,10 +97,23 @@ class Paginas(ListPageSource):
         embed = Embed(title = "Giphy!", 
                       description = f"**Resultados para:** *{self.busqueda}*" + "\n" + f"**Enlace:** https://giphy.com/gifs/{url[31 : longitud_codigo]}", 
                       colour = self.ctx.author.colour, timestamp = datetime.utcnow())
-        embed.set_footer(text = f"Solicitado por {self.ctx.author.display_name}" + "\n" + f"{offset:,} de {len_data:,} gifs.", icon_url = self.ctx.author.avatar_url)
+        embed.set_footer(text = f"Solicitado por {self.ctx.author.display_name}" + "\n" + f"{offset:,} de {len_data:,} resultados.", icon_url = self.ctx.author.avatar_url)
         
         embed.set_image(url= str(url))
         return embed
+
+
+class BusquedaYoutube(ListPageSource):
+    def __init__(self, ctx, data):
+        self.ctx = ctx
+        self.urls = data
+        super().__init__(data, per_page = 1)
+
+    def format_page(self, menu, entries):
+        offset = (menu.current_page * self.per_page) + 1
+        len_data = len(self.entries)
+        url = "https://www.youtube.com/watch?v=" + self.urls[menu.current_page][0] + "\n" + f"**Solicitado por {self.ctx.author.display_name}**" + " | " + f"*{offset:,} de {len_data:,} videos.*"
+        return url 
 
 
 def setup(bot):
